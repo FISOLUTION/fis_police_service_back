3f9cde8b9e90729eca7ef919401b47694e30ac27
seungbum Lee <seungbumlee@seungbumui-MacBookAir.local>
2022-01-10 15:07:24 +0900

    feat: saveAgent

--- fis_police_server/src/main/java/fis/police/fis_police_server/service/serviceImpl/AgentServiceImpl.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/service/serviceImpl/AgentServiceImpl.java
@@ -1,22 +1,32 @@
 package fis.police.fis_police_server.service.serviceImpl;
 
 import fis.police.fis_police_server.domain.Agent;
+import fis.police.fis_police_server.repository.AgentRepository;
 import fis.police.fis_police_server.service.AgentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class AgentServiceImpl implements AgentService {
-    @Override
-    public Boolean saveAgent() {
-        return null;
+
+    private final AgentRepository agentRepository;
+
+    @Override // 현장요원 추가
+    public void saveAgent(Agent agent) {
+        agentRepository.save(agent);
     }
 
-    @Override
+    @Override // 현장요원 수정
     public Boolean modifyAgent() {
         return null;
     }
 
-    @Override
+    @Override // 현장요원 조회
     public List<Agent> getAgent() {
         return null;
     }
