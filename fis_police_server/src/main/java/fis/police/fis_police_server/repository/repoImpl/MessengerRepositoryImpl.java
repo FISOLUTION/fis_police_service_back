d905f085f5866ffa9dae5de3044b0b70a6e45592
digda-5624 <hsg0208@naver.com>
2022-01-24 16:15:51 +0900

    distinct problem

--- fis_police_server/src/main/java/fis/police/fis_police_server/repository/repoImpl/MessengerRepositoryImpl.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/repository/repoImpl/MessengerRepositoryImpl.java
@@ -47,7 +47,7 @@ public class MessengerRepositoryImpl implements MessengerRepository {
 
     @Override
     public List<Messenger> findByUser(User user) {
-        return em.createQuery("select messenger from Messenger messenger " +
+        return em.createQuery("select distinct messenger from Messenger messenger " +
                 "join fetch messenger.user on messenger.user.id = :id", Messenger.class)
                 .setParameter("id", user.getId())
                 .getResultList();
