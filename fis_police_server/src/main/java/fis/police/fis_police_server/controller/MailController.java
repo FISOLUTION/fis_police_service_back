2a5af10e35bb0390e8d91f5393b78583d6522550
고준영 <junyeong@junyeong.local>
2022-02-03 16:28:33 +0900

    jun log

--- fis_police_server/src/main/java/fis/police/fis_police_server/controller/MailController.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/controller/MailController.java
@@ -4,10 +4,11 @@ import fis.police.fis_police_server.dto.MailSendRequest;
 import fis.police.fis_police_server.dto.MailSendResponse;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
 
 // 고준영
 public interface MailController {
 
     // 시설에 메일 전송
-    MailSendResponse sendMail(Long center_id) throws MessagingException;
+    MailSendResponse sendMail(Long center_id, HttpServletRequest request) throws MessagingException;
 }
--- fis_police_server/src/main/java/fis/police/fis_police_server/domain/Call.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/domain/Call.java
@@ -65,8 +65,8 @@ public class Call {
     작성 내용: service 계층에서 받아온 api 스펙을 바탕으로 call 객체 생성하는 생성자와 메서드
 */
     public Call(Center center, User user, String date, String time, Participation participation, InOut in_out, String c_manager, String m_ph, String m_email, Integer num, String center_etc, String agent_etc) {
-        this.center = center;
-        this.user = user;
+        this.mappingCenter(center);
+        this.mappingUser(user);
         this.date = date;
         this.time = time;
         this.participation = participation;
@@ -85,6 +85,20 @@ public class Call {
                 request.getCenter_etc(), request.getAgent_etc());
 
         return call;
+    }
+/*
+    작성 날짜: 2022/02/03 3:25 오후
+    작성자: 고준영
+    작성 내용: 연관관계 메서드
+*/
+    public void mappingCenter(Center center) {
+        this.center = center;
+        center.getCallList().add(this);
+    }
+
+    public void mappingUser(User user) {
+        this.user = user;
+        center.getCallList().add(this);
     }
 
     /*
