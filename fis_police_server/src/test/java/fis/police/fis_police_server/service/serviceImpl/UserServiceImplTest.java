1c3a205b5cd3d14d1e351e2cbd11bf75bac04438
wonbora <bora4057@naver.com>
2022-02-04 10:39:35 +0900

    aa

--- fis_police_server/src/test/java/fis/police/fis_police_server/service/serviceImpl/UserServiceImplTest.java
+++ fis_police_server/src/test/java/fis/police/fis_police_server/service/serviceImpl/UserServiceImplTest.java
@@ -6,6 +6,7 @@ import fis.police.fis_police_server.domain.enumType.UserAuthority;
 import fis.police.fis_police_server.dto.UserSaveRequest;
 import fis.police.fis_police_server.repository.UserRepository;
 import fis.police.fis_police_server.service.UserService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,18 +32,24 @@ class UserServiceImplTest {
     @Test
     public void 회원가입_닉네임_중복() throws Exception {
         //given
-//        final UserSaveRequest saveDto1 = new UserSaveRequest(1L, "fis001", "원보라", "1234", "010-0000-0001", LocalDate.of(2021,01,01), UserAuthority.ADMIN);
-//        final UserSaveRequest saveDto2 = new UserSaveRequest(2L, "fis001", "원보라2", "1234", "010-0000-0002", LocalDate.of(2021,01,02), UserAuthority.USER);
-//
-//        //when
+        final UserSaveRequest saveDto1 = new UserSaveRequest(1L, "fis001", "원보라", "1234", "010-0000-0001", LocalDate.of(2021,02,04), UserAuthority.ADMIN);
+        final UserSaveRequest saveDto2 = new UserSaveRequest(2L, "fis001", "원보라2", "1234", "010-0000-0002", LocalDate.of(2021,02,04), UserAuthority.USER);
+
+        //when
 //        userService.saveUser(saveDto1);
 //        try {
 //            userService.saveUser(saveDto2);    // 예외 발생
 //        } catch (IllegalStateException e) {
-//            System.out.println("예외발생============================");
+//            System.out.println("닉네임 중복============================");
+//
 //        }
-//        //then
-//        System.out.println(saveDto1.getU_nickname());
-//        System.out.println(saveDto2.getU_nickname());
+
+
+        userService.saveUser(saveDto1);
+        userService.saveUser(saveDto2);    // 예외 발생
+
+        fail("중복");
+        //then
+//        Assertions.assertThat(saveDto1.getU_nickname()).isEqualTo(saveDto2.getU_nickname());
     }
 }
