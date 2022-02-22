211fe98dc055d77ec5f051a9cbdd5be92dccdf2f
wonbora <bora4057@naver.com>
2022-02-22 15:52:28 +0900

    accept list

--- fis_police_server/src/main/java/fis/police/fis_police_server/dto/AppAcceptScheduleRequest.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/dto/AppAcceptScheduleRequest.java
@@ -5,10 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AppAcceptScheduleRequest {
-    private Long schedule_id;
+    private List<Long> schedule_id;
     private Accept accept;
 }
