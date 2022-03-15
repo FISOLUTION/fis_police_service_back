789a99b46b6b5d4317778f1b367de13fa78ade05
고준영 <junyeong@junyeong.local>
2022-03-15 15:32:45 +0900

    conflict merge again

--- fis_police_server/src/main/java/fis/police/fis_police_server/controller/controllerImpl/AppLoginControllerImpl.java
+++ fis_police_server/src/main/java/fis/police/fis_police_server/controller/controllerImpl/AppLoginControllerImpl.java
@@ -40,14 +40,11 @@ public class AppLoginControllerImpl implements AppLoginController {
         log.info("[로그인 역할: {}]", loginRequest.getRole());
 
         // 토큰 생성
-<<<<<<< HEAD
         String token = tokenService.createToken(primaryKey, loginResponse, "access");  // accessToken 생성 (유효시간 30분)
         String refreshToken = tokenService.createToken(primaryKey, loginResponse, "refresh");   // refreshToken 생성 (유효시간 7일)
 
-=======
-        String token = tokenService.makeToken(loginUserId, loginResponse);
->>>>>>> 2c94355c13355e4d19b65314fa17fd8a88867824
         loginResponse.setToken(token);
+        loginResponse.setRefreshToken(refreshToken);
 
         return loginResponse;
     }
